6a356ac714741ab3ebf0247501023a197e857b00
MarcosE <maedaes24@gmail.com>
2023-05-27 01:43:32 -0400

    forms added to admin panel

--- lesTourApp/admin.py
+++ lesTourApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Empleados)
+admin.site.register(models.Clientes)
+admin.site.register(models.Reservas)
+admin.site.register(models.Hoteles)
+admin.site.register(models.Habitacion)
+admin.site.register(models.Tipo_Habitacion)
+admin.site.register(models.Reserva_Huesped)
+admin.site.register(models.Puesto_Trabajo)
+admin.site.register(models.Cargo)
+admin.site.register(models.Areas)
