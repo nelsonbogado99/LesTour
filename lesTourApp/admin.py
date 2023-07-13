e40491e3da2615dd662a8d64698681cc9138b287
maurogimenezb <maurogimenezb@gmail.com>
2023-07-13 13:12:54 -0400

    13/7 - Ordenamiento de tablas

--- lesTourApp/admin.py
+++ lesTourApp/admin.py
@@ -1,14 +1,47 @@
-from django.contrib import admin
 from . import models
+from django.contrib import admin
+from .models import Ciudades, Empleados, Clientes, Reservas, Hoteles, Habitacion, Tipo_Habitacion, Reserva_Huesped, Puesto_Trabajo, Cargo, Areas
 
-admin.site.register(models.Ciudades)
-admin.site.register(models.Empleados)
-admin.site.register(models.Clientes)
-admin.site.register(models.Reservas)
-admin.site.register(models.Hoteles)
-admin.site.register(models.Habitacion)
-admin.site.register(models.Tipo_Habitacion)
-admin.site.register(models.Reserva_Huesped)
-admin.site.register(models.Puesto_Trabajo)
-admin.site.register(models.Cargo)
-admin.site.register(models.Areas)
+@admin.register(Ciudades)
+class CiudadesAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
+
+@admin.register(Empleados)
+class EmpleadosAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
+
+@admin.register(Clientes)
+class ClientesAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
+
+@admin.register(Reservas)
+class ReservasAdmin(admin.ModelAdmin):
+    ordering = ['id_cliente']
+
+@admin.register(Hoteles)
+class HotelesAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
+
+@admin.register(Habitacion)
+class HabitacionAdmin(admin.ModelAdmin):
+    ordering = ['numero']
+
+@admin.register(Tipo_Habitacion)
+class TipoHabitacionAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
+
+@admin.register(Reserva_Huesped)
+class ReservaHuespedAdmin(admin.ModelAdmin):
+    ordering = ['id_cliente']
+
+@admin.register(Puesto_Trabajo)
+class PuestoTrabajoAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
+
+@admin.register(Areas)
+class AreasAdmin(admin.ModelAdmin):
+    ordering = ['nombre']
