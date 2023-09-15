48daeb0551fcff42230be10b6bdfd4c5d5554c17
MarcosE24 <42250217+MarcosE24@users.noreply.github.com>
2023-09-15 00:40:04 -0400

    error message improvements

--- lesTourWeb/views.py
+++ lesTourWeb/views.py
@@ -4,23 +4,32 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 
-# Create your views here.
 def home(request):
     return render(request, "home.html")
 
+
 def signUp(request):
     if request.method == "GET":
-        return render(request, "signUp.html",{
-        "form":UserCreationForm
-        })
+        return render(request, "signUp.html", {"form": UserCreationForm})
     else:
         if request.POST["password1"] == request.POST["password2"]:
-            #Register user
+            # Register user
             try:
-                user= User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
                 user.save()
                 return HttpResponse("Registro completado satisfactoriamente")
             except:
-                return HttpResponse("Nombre de usuario ya existe")
+                return render(
+                    request,
+                    "signUp.html",
+                    {"form": UserCreationForm, "error": "El usuario ya existe"},
+                )
         else:
-            return HttpResponse("Las Contraseñas no coinciden")
+            return render(
+                request,
+                "signUp.html",
+                {"form": UserCreationForm, "error": "Las contraseñas no coinciden"},
+            )
