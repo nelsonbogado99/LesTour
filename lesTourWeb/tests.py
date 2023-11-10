28ac4f9747b921493c3dc97f077ffdf5b928d76b
nelsonbogado99 <80130879+nelsonbogado99@users.noreply.github.com>
2023-11-10 16:36:14 -0300

    Se agrega test de las vistas creadas

--- lesTourWeb/tests.py
+++ lesTourWeb/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class MiAppVistaTests(TestCase):
+
+    def test_vista_hoteles_funciona_correctamente(self):
+        url = reverse('hoteles')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_url_personal_resuelve_correctamente(self):
+        url = reverse('personal')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_url_usuario_resuelve_correctamente(self):
+        url = reverse('usuario')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
